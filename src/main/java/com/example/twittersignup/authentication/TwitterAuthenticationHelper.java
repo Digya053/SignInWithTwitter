9918f4108bbc542012f687cfb254f09975e1557f
Digya <digyaacharyaa@gmail.com>
2017-08-14 15:05:34 +0545

    Remove version

--- src/main/java/com/example/twittersignup/authentication/TwitterAuthenticationHelper.java
+++ src/main/java/com/example/twittersignup/authentication/TwitterAuthenticationHelper.java
@@ -26,9 +26,6 @@ public class TwitterAuthenticationHelper {
 	@Value("${oauth.nonce}")
 	private String nonce;
 
-	@Value("${oauth.version}")
-	private String version;
-
 	@Value("${oauth.signature}")
 	private String signature;
 
@@ -43,7 +40,6 @@ public class TwitterAuthenticationHelper {
 				+ ",oauth_signature_method="+signatureMethod
 				+ ",oauth_timestamp="+timeStamp
 				+ ",oauth_nonce="+nonce
-				+ ",oauth_version="+version
 				+ ",oauth_signature="+signature);
 
 		String response = HTTPUtils.sendPostWithHeaders(refreshTokenUrl, headers);
